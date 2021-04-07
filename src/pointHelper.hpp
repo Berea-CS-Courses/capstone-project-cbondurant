cd68dc45a948468294e5c6ff5171bcd2c03632a8
Conner Bondurant <conner.bondurant@triptych.me>
2021-04-07 17:53:48 -0400

    Add new testing framework and tests

--- src/pointHelper.hpp
+++ src/pointHelper.hpp
@@ -3,11 +3,14 @@
 
 #include <QPointF>
 #include <cmath>
+#include <algorithm>
 
 namespace Lipuma{
 	/*
 		Linear Interpolation
 		from x=0 => x=1 interpolates from a to b
+		This function extrapolates for values outside of [0,1]
+		Clamp your inputs if clamped behavior is needed.
 	*/
 	QPointF lerp(QPointF a, QPointF b, qreal x){
 		return (a*(1-x) + b* x);
@@ -17,6 +20,7 @@ namespace Lipuma{
 		Normalizes a 2d vector represented as a QPointF to unit length
 	*/
 	QPointF normalize(QPointF p){
+		if (p.isNull()) return QPointF();
 		qreal d = sqrt(abs(QPointF::dotProduct(p,p)));
 		return p/d;
 	}
